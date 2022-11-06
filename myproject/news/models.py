5da237ca83770242658367e24e9f182dcd9cf912
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-11-06 12:10:21 +0100

    create models for app news and migrations

--- myproject/news/models.py
+++ myproject/news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Giornalista(models.Model):
+    nome=models.CharField(max_length=30)
+    cognome=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.nome+" "+self.cognome
+
+
+class Articolo(models.Model):
+    titolo=models.CharField(max_length=40)
+    contenuto=models.TextField()
+    giornalista=models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
+
+    def __str__(self):
+        return self.titolo
